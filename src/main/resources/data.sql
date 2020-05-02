74bd6049cef03512bd00f1e56860479b0944a4d4
yuuuyatamura <49576718+yuuuyatamura@users.noreply.github.com>
2020-05-02 20:46:38 +0900

    Modify

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,8 @@
 /*従業員テーブルのデータ */
-INSERT INTO employee (employee_id,
-employee_name,age)
-VALUES(1,'山田太郎',30);
+INSERT INTO employee (employee_id,employee_name,age) VALUES(1,'山田太郎',30);
+INSERT INTO employee (employee_id,employee_name,age) VALUES(2,'山田2世',32);
+INSERT INTO employee (employee_id,employee_name,age) VALUES(3,'George',12);
+INSERT INTO employee (employee_id,employee_name,age) VALUES(4,'田中拓郎',23);
+INSERT INTO employee (employee_id,employee_name,age) VALUES(5,'田島裕太',40);
+INSERT INTO employee (employee_id,employee_name,age) VALUES(6,'中村 華',28);
+INSERT INTO employee (employee_id,employee_name,age) VALUES(7,'松澤 圭介'26);
