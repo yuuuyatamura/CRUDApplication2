7f822a5ad3eff67601b4644d8cc43975df9f3b3c
yuuuyatamura <49576718+yuuuyatamura@users.noreply.github.com>
2020-05-03 01:14:10 +0900

    スペルミス修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,4 +5,4 @@ INSERT INTO employee (employee_id,employee_name,age) VALUES(3,'George',12);
 INSERT INTO employee (employee_id,employee_name,age) VALUES(4,'田中拓郎',23);
 INSERT INTO employee (employee_id,employee_name,age) VALUES(5,'田島裕太',40);
 INSERT INTO employee (employee_id,employee_name,age) VALUES(6,'中村 華',28);
-INSERT INTO employee (employee_id,employee_name,age) VALUES(7,'松澤 圭介'26);
+INSERT INTO employee (employee_id,employee_name,age) VALUES(7,'松澤 圭介',26);
